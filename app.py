86b72275105e863e2913df5960b5aa2dae12f0c3
alt-0191 <56725325+alt-0191@users.noreply.github.com>
2023-06-15 18:32:53 +0800

    add login&user manage
    not yet...

--- app.py
+++ app.py
@@ -6,15 +6,36 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy()
 
+'''定义Phone模型用户添加删除'''
+
 
 class Phone(db.Model):
-    phonebrand = db.Column(db.String(10), nullable=False, primary_key=True)
+    id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
+    phonebrand = db.Column(db.String(10))
     phonetype = db.Column(db.String(100), nullable=False)
     cpuinfo = db.Column(db.String(20), nullable=False)
     ramandrominfo = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
-        return '%r,%r,%r,%r' % (self.phonebrand, self.phonetype, self.cpuinfo, self.ramandrominfo)
+        return '%r,%r,%r,%r,%r' % (self.id, self.phonebrand, self.phonetype, self.cpuinfo, self.ramandrominfo)
+
+
+'''定义User模型用于权限管理'''
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(120))
+    role = db.Column(db.String(80))
+
+    def __init__(self, username, password, role):
+        self.username = username
+        self.password = password
+        self.role = role
+
+    def __repr__(self):
+        return '%r,%r,%r' % (self.username, self.password, self.role)
 
 
 # 连接数据库
@@ -31,14 +52,60 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    if 'role' in session:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        print(f"从前端获取到的账号密码为: 用户名={username}, 密码={password}")
+        user = User.query.filter_by(username=username).first()
+        print(f"数据库中查询到的用户名和密码为: 用户名={username}, 密码={user.password}")
+        print(user, username)
+        user_str = (str(user)).split(',')
+        print(user_str[0])
+        if user_str[0].strip("'") == username:
+            if user_str[1].strip("'") == password:
+                print("账号密码正确")
+                session['role'] = user.role
+                print(f"已设置权限组为{user.role}")
+                return redirect(url_for('index'))
+            else:
+                print("密码错误")
+                return render_template('login.html', error='密码错误')
+        else:
+            print("用户不存在")
+            return render_template('login.html', error='用户名不存在')
+    else:
+        print("登陆成功，跳转")
+        return render_template('login.html')
 
 
 @app.route("/add_phone")
-def add_student():
-    return render_template("add_phone.html")
+def add_phone():
+    if 'role' in session and session['role'] == 'admin':
+        return render_template('add_phone.html')
+    else:
+        return '您没有权限执行此操作'
+
+
+@app.route("/delete_phone", methods=["POST"])
+def delete_phone():
+    if 'role' in session and session['role'] == 'admin':
+        id = request.form["id"]
+        phone = Phone.query.filter_by(id=id).first()
+        db.session.delete(phone)
+        db.session.commit()
+        return "success"
+    else:
+        return '您没有权限执行此操作'
 
 
 @app.route("/saverecord", methods=["POST", "GET"])
@@ -68,33 +135,45 @@ def phone_info():
     for phone in phones:
         parts = phone.split(',')
         result.append({
-            "phonebrand": parts[0],
-            "phonetype": parts[1],
-            "cpuinfo": parts[2],
-            "ramandrominfo": parts[3]
+            "id": parts[0],
+            "phonebrand": parts[1],
+            "phonetype": parts[2],
+            "cpuinfo": parts[3],
+            "ramandrominfo": parts[4]
 
         })
     phones_json = json.dumps(result, ensure_ascii=False, sort_keys=False, indent=4, separators=(',', ':')).strip('[]')
     print("json数据" + phones_json)
     return render_template('phone_info.html', phones_json=phones_json)
 
+
 @app.route("/update_phone", methods=["POST"])
 def update_phone():
+    id = request.form["id"]
+    phone = Phone.query.filter_by(id=id).first()
+    print(phone)
     phonebrand = request.form["phonebrand"]
     phonetype = request.form["phonetype"]
     cpuinfo = request.form["cpuinfo"]
     ramandrominfo = request.form["ramandrominfo"]
 
     # 更新数据库中的数据
-    phone = Phone.query.filter_by(phonebrand=phonebrand).first()
+    print(phone)
+    phone.phonebrand = phonebrand
+    print(phonebrand)
     phone.phonetype = phonetype
+    print(phonetype)
     phone.cpuinfo = cpuinfo
+    print(cpuinfo)
     phone.ramandrominfo = ramandrominfo
+    print(ramandrominfo)
     db.session.commit()
 
     return "success"
 
 
-
 if __name__ == "__main__":
     app.run(debug=True)
+    app.config['SECRET_KEY'] = 'SDN32NLFDO3JDFJD'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
